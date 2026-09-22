273bc11c6c4aef1c6fbdbc06eec678e647208bcd
madmax97so2 <pavansivalu@gmail.com>
2026-09-22 13:20:39 +0530

    Add division class with main method for division

---
+++ division.java
@@ -0,0 +1,12 @@
+public class division {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 5;
+
+        int result = a / b;
+
+        System.out.println("Division = " + result);
+    }
+}
